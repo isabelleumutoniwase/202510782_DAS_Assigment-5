a243770e7c93f11a1327b74cb5f6d7632ba20f5e
isabelleumutoniwase <isabelleumutoniwase403@gmail.com>
2026-05-31 08:37:46 +0200

    Add header file for library book functions

---
+++ Functions .h
@@ -0,0 +1,14 @@
+#ifndef FUNCTIONS_H
+#define FUNCTIONS_H
+
+#include "LibraryBook.h"
+
+void addBook();
+void displayBooks();
+void searchBook();
+void borrowBookMenu();
+void returnBookMenu();
+void displayOverdueBooks();
+void calculateTotalFines();
+
+#endif
